f376309b5d8985198cddde402514385bf0d341a0
AlexWolf2077 <145503441+AlexWolf2077@users.noreply.github.com>
2023-11-28 12:34:49 +0300

    модальное окно

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/* Модальное окно */
+document.addEventListener('DOMContentLoaded', function() {
+  const modalOverlay = document.querySelector('.modal-overlay');
+  modalOverlay.style.display = 'none';
+});
+const openModalBtn = document.querySelector('.open-modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModalBtn = document.querySelector('.close-modal-btn');
+const contactForm = document.getElementById('contact-form');
+const nameInput = document.getElementById('name');
+const phoneInput = document.getElementById('phone');
+openModalBtn.addEventListener('click', function() {
+  modalOverlay.style.display = 'flex';
+});
+closeModalBtn.addEventListener('click', function() {
+  closeModal();
+});
+contactForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (validateForm()) {
+    // Отправка формы
+    closeModal();
+  }
+});
+modalOverlay.addEventListener('click', function(event) {
+  if (event.target === modalOverlay) {
+    closeModal();
+  }
+});
+function validateForm() {
+  const nameValue = nameInput.value.trim();
+  const phoneValue = phoneInput.value.trim();
+  if (nameValue === '' || phoneValue === '') {
+    alert('Пожалуйста, заполните все поля формы.');
+    return false;
+  }
+  if (!/^[a-zA-Zа-яА-Я\s]+$/.test(nameValue)) {
+    alert('Поле "Имя" должно содержать только текстовые значения.');
+    return false;
+  }
+  if (!/^\d+$/.test(phoneValue)) {
+    alert('Поле "Номер телефона" должно содержать только числовые значения.');
+    return false;
+  }
+  return true;
+}
+function closeModal() {
+  modalOverlay.style.display = 'none';
+  contactForm.reset();
+}
