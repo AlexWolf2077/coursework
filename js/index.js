73a470e0d0b7d7ff6da7dc5b99bdf390c76a0645
AlexWolf2077 <145503441+AlexWolf2077@users.noreply.github.com>
2024-04-22 15:56:27 +0300

    node.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,33 @@
-let a = 5;
-let b = 10;
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
-console.log(a + b, 'Hello');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello 555');
+});
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Пользователь',
+    }, 
+    'secret123'
+);
+
+    res.json({
+        success: true,
+        token,
+    });
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('server ok');
+});
